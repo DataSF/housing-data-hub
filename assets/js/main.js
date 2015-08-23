e2668946f62e9b41cef1b850cc383193cdd3cc0c
Jason Lally <jason@placematters.org>
2015-08-23 18:45:56 +0000

    clean up layouts, now down to 3

--- assets/js/main.js
+++ assets/js/main.js
@@ -743,20 +743,3 @@ function toGeojson(json, locationField) {
     return geojson;
   }
 };
-
-</script>
-<script>
-var $buoop = {
-  c: 2
-};
-
-function $buo_f() {
-  var e = document.createElement("script");
-  e.src = "//browser-update.org/update.js";
-  document.body.appendChild(e);
-};
-try {
-  document.addEventListener("DOMContentLoaded", $buo_f, false)
-} catch (e) {
-  window.attachEvent("onload", $buo_f)
-}
